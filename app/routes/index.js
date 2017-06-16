26d9258686db6cdc414999ccd66a0ffa2b7557c4
johanhenrikssn <johan.c.henriksson@gmail.com>
2017-06-16 22:51:09 +0200

    Remove decordeURI parsing

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,15 +4,14 @@ const rss = require('../services/rss')();
 
 router.get('/episodes', (req, res) => {
 	const { feedurl } = req.query;
-	const decodedUrl = decodeURIComponent(feedurl);
 
 	rss
-		.fetchData(decodedUrl)
+		.fetchData(feedurl)
 		.then(episodes => {
 			res.status(200).send(episodes);
 		})
 		.catch(err => {
-			res.status(err.status).send(err.desc);
+			res.status(500).send(err.message);
 		});
 });
 
